f37056de705cf1dad765a69c4e55c488e15241af
beMimg <bernardommguerreiro@gmail.com>
2024-03-17 12:19:57 +0100

    authMiddleware created, checks if user has authenticated token

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+exports.isAuthenticated = async (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token === null) res.sendStatus(401);
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
